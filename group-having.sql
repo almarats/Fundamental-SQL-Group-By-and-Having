0019944eb5c0d036563cc3553f4c163d7340db42
Almar <44309017+almaratsn@users.noreply.github.com>
2022-02-03 11:45:15 +0800

    Update group-having.sql

--- group-having.sql
+++ group-having.sql
@@ -5,7 +5,8 @@ GROUP BY customer_id
 HAVING SUM(pinalty) > 20000
 
 #Mencari customer yang mengganti layanan
-SELECT t1.Name AS name, GROUP_CONCAT(t3.product_name) FROM customer t1 JOIN subscription t2 ON t1.id = t2.customer_id JOIN product t3 ON t2.product_id =t3.ID WHERE t1.id IN (
+SELECT t1.Name AS name, GROUP_CONCAT(t3.product_name) FROM customer t1 JOIN subscription t2 ON t1.id = t2.customer_id 
+JOIN product t3 ON t2.product_id =t3.ID WHERE t1.id IN (
 	SELECT customer_id FROM subscription GROUP BY customer_id 
   	HAVING COUNT(customer_id) >1 
 )
